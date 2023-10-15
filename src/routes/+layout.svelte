c69a0f189537b8d1dfab84e8c2ff9b8f6bdc548f
aurelienroux <aurelien.roux@busbud.com>
2023-10-14 21:45:10 -0400

    layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	// export let data;
+</script>
+
+<nav>
+	<a href="/">home</a>
+	<a href="/about">about</a>
+</nav>
+
+<main>
+	<slot />
+</main>
---
+++ src/routes/+layout.ts
@@ -0,0 +1,5 @@
+export function load() {
+	return {
+		layout: 'data from layout'
+	};
+}
