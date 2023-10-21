27aa727a9638ef1092d8eb512a4fe9d5c1b874e9
aurelienroux <aurelien.roux@busbud.com>
2023-10-20 21:04:28 -0400

    display page data

---
+++ src/routes/blog/[slug]/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import { page } from '$app/stores';
+
+	export let data: PageData;
+</script>
+
+<h2>article: {$page.params.slug}</h2>
+<h2>article: {$page.data.layout}</h2>
