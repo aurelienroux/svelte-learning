a5611d1ba00cab34259b3806def8f60d7b494295
aurelienroux <aurelien.roux@busbud.com>
2023-10-24 22:39:37 -0400

    display post with layout server

--- src/routes/blog/+layout.server.ts
+++ src/routes/blog/+layout.server.ts
@@ -1,12 +1,10 @@
-import type { PageServerLoad } from './$types';
-
 import { posts } from '$lib/data';
 
-export const load = (async () => {
+export const load = async () => {
 	return {
 		summaries: posts.map((post) => ({
 			title: post.title,
 			slug: post.slug
 		}))
 	};
-}) satisfies PageServerLoad;
+};
